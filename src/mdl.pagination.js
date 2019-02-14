89c021388d76c4f275dd4b50d9861a63aa111e5c
Rupert Ong <rupert.ong@gmail.com>
2019-02-14 11:12:25 -0500

    Add click event handlers to Pagination component

--- src/mdl.pagination.js
+++ src/mdl.pagination.js
@@ -13,18 +13,45 @@
     };
 
     var config = _NS.utils.extend(true, defaultConfig, userConfig);
+    config.totalPages = Math.ceil(config.total / config.limit);
 
     this.selector = selector;
     this.state = {
       config: config
     };
-
+    this._addEventHandlers();
     this._render();
   }
 
   Pagination.prototype = {
-    _render: function(){
-      document.querySelector(this.selector).innerHTML = _NS.template.render(Pagination.template, null);
+    _addEventHandlers: function() {
+      document.querySelector(this.selector).addEventListener(
+        "click",
+        function(e) {
+          var classTokenList = e.target.classList,
+            stateConfig = this.state.config;
+          if (classTokenList.contains("_mdl-pagination-first")) {
+            stateConfig.current = 1;
+          } else if (classTokenList.contains("_mdl-pagination-prev")) {
+            stateConfig.current = Math.max(1, stateConfig.current - 1);
+          } else if (classTokenList.contains("_mdl-pagination-next")) {
+            stateConfig.current = Math.min(
+              stateConfig.totalPages,
+              stateConfig.current + 1
+            );
+          } else if (classTokenList.contains("_mdl-pagination-last")) {
+            stateConfig.current = stateConfig.totalPages;
+          }
+          console.log(stateConfig.current);
+          // update UI to reflect state
+        }.bind(this)
+      );
+    },
+    _render: function() {
+      document.querySelector(this.selector).innerHTML = _NS.template.render(
+        Pagination.template,
+        null
+      );
     }
   };
 
